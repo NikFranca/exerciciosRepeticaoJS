b4529209b6eca7ee2288556754667d1e458e3c9c
NikFranca <140428664+NikFranca@users.noreply.github.com>
2025-11-18 14:24:57 -0300

    Add files via upload

---
+++ Dcondicionais.js
@@ -0,0 +1,38 @@
+/*let fim = prompt("qual é o dia da semana? ");
+console.log(fim);
+
+if(fim === "Domingo" || fim === "Sabado"){
+    alert("Bom fim de semana!");
+    console.log("fim de semana");
+}else{
+    alert("Boa semana!");
+    console.log("semana");
+}
+*/
+
+let numero = number(prompt("digite um numero: "));
+
+if( numero > 0 ){
+   alert(`numero ${numero} é positivo`);
+}else{
+    alert(`numero ${numero} é negativo`);
+}
+
+
+/*
+let pontuacao = prompt("digite o valor da sua pontuação: ");
+
+if( pontuacao >= 100 ){
+    alert("Parabéns, você venceu!");
+}else{
+    alert("Tente novamente para ganhar.");
+}
+
+let saldoConta = 500; // Exemplo de saldo
+alert(`Seu saldo é de R$${saldoConta}.`);
+
+
+let usuario = prompt("digite o seu nome: ");
+
+alert(`Bem vindo ${usuario} ao jogo de adivinhação!`);
+*/
