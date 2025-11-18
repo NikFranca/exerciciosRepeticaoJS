a183be7fe47cc21f0d48f65491cdae2b02ca487a
NikFranca <140428664+NikFranca@users.noreply.github.com>
2025-11-18 14:34:50 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,73 @@
+/*let temPao = prompt("Tem pão na cesta? (sim/não)");
+
+if (temPao === 'sim') {
+    console.log("Café da manhã garantido!");
+} elif (temPao === 'não' || temPao === 'nao'); {
+    console.log("Sem pão, sem café da manhã.");
+} else {
+    alert("Resposta inválida. Por favor, responda com 'sim' ou 'não'.");
+}
+
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = qtdNumeros;
+
+while(contador > 0) {
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
+    contador--;
+}
+
+let media = soma / qtdNumeros;
+
+console.log(media);
+
+let contador = 10;
+
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
+
+let numero = prompt("Digite um número: ");
+
+while(numero >= 0 ){
+    console.log(numero);
+    numero--;
+}
+
+let numero = prompt("Digite um número pra contagem regressiva : ");
+let contador = 0 
+
+while(numero >= contador){
+    console.log(numero);
+    numero--;
+}*/
+/*
+let nome = prompt("Qual seu nome?");
+console.log(`Olá, ${nome}`);
+alert(`Olá, ${nome}`);
+let linguagem = prompt("Qual sua linguagem de programação favorita?");
+console.log(`${linguagem} que top!`)
+alert(`${linguagem} que top!`);
+
+
+let n1 = prompt("Digite um número:");
+let n2 = prompt("Digite outro número:");
+let resultado = parseInt(n1) - parseInt(n2);
+alert(`O resultado da subtração entre ${n1} e ${n2} é: ${resultado}`);
+
+let idade = prompt("Qual sua idade?");
+
+if(idade >= 18){
+    alert(`você tem ${idade} anos, então é maior de idade!`);
+}else{
+    alert(`você tem ${idade} anos, então é menor de idade!`)
+}
+
+let n = parseInt(Math.random() * 1000 + 1);
+alert(n);*/
+
+let numeroAleatorio = Math.random();
+console.log(numeroAleatorio);
+alert(numeroAleatorio);
